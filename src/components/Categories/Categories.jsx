5ca893168fc0042b499693687f212c2e4a1ea3ea
Nick <nick92dan98@gmail.com>
2024-08-21 22:13:15 +0300

    add filtering by categories

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import styles from "./styles.module.css";
+export const Categories = ({
+    categories,
+    setSelectedCategory,
+    selectedCategory,
+}) => {
+    return (
+        <div className={styles.categories}>
+            {categories.map((category) => {
+                return (
+                    <button
+                        className={
+                            selectedCategory === category
+                                ? styles.active
+                                : styles.item
+                        }
+                        key={category}
+                        onClick={() => setSelectedCategory(category)}
+                    >
+                        {category}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
