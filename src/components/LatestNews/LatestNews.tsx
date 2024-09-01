8b320c0309e4551ac240ba30090144b58fb08322
Nick <nick92dan98@gmail.com>
2024-09-02 01:16:05 +0300

    migrate from JS to TS

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { getLatestNews } from "../../api/apiNews";
 import { useFetch } from "../../helpers/hooks/useFetch";
+import { NewsApiResponse } from "../../interfaces";
 import BannersListWithSkeleton from "../BannersList/BannersList";
 import styles from "./styles.module.css";
 export const LatestNews = () => {
-    const { data, isLoading } = useFetch(getLatestNews);
+    const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNews);
 
     return (
             <section className={styles.section}>
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
 import React, { useRef } from "react";
 import styles from "./styles.module.css";
-export const Slider = ({ children, step = 150 }) => {
-    const sliderRef = useRef(null);
+
+interface Props {
+    children: React.ReactElement
+    step?: number
+}
+export const Slider = ({ children, step = 150 }: Props) => {
+    const sliderRef = useRef<HTMLElement | null>(null);
     const scrollLeft = () => {
+        if(!sliderRef.current) return
         sliderRef.current.scrollLeft -= step;
     };
     const scrollRight = () => {
+        if(!sliderRef.current) return
         sliderRef.current.scrollLeft += step;
     };
     return (
